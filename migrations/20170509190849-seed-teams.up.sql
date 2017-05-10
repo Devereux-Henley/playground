0032de492eb1158ab6f15d9c398b21413a3ba700
Devereux Henley <devereux@ksu.edu>
2017-05-09 19:46:24 -0500

    Added team and team-member seedings.

---
+++ migrations/20170509190849-seed-teams.up.sql
@@ -0,0 +1,8 @@
+-- migration to be applied
+INSERT INTO teams (organization_id, name)
+VALUES (1, 'Requirement Making Fools'),
+       (2, 'React Ninjas'),
+       (2, 'Virtual React Monsters'),
+       (2, 'Facebook Vigilantes'),
+       (2, 'Oculus Movie-Goers'),
+       (3, 'Random Team Of Destiny');
---
+++ migrations/20170509194346-seed-team-members.up.sql
@@ -0,0 +1,8 @@
+-- migration to be applied
+INSERT INTO team_members (user_id, team_id)
+VALUES (1, 1),
+       (1, 2),
+       (1, 3),
+       (1, 5),
+       (2, 1),
+       (2, 5);
